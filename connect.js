f6dae89f1ef1956aeb39737add7411a7ee6e145a
Unnat Mittal <unnatmittal2000@gmail.com>
2023-02-01 19:59:40 +0530

    1.0.3

--- connect.js
+++ connect.js
@@ -1,14 +1,14 @@
 import mysql from "mysql2";
 
-// export const db = mysql.createConnection({
-//   user: "root",
-//   host: "127.0.0.1",
-//   password: "Mittal@123",
-//   database: "nikola",
-// });
 export const db = mysql.createConnection({
-  user: "admin",
-  host: "database-1.cuzsb9f6gfsb.ap-south-1.rds.amazonaws.com",
-  password: "87654321",
-  database: "Nikola",
+  user: "root",
+  host: "127.0.0.1",
+  password: "Mittal@123",
+  database: "nikola",
 });
+// export const db = mysql.createConnection({
+//   user: "admin",
+//   host: "database-1.cuzsb9f6gfsb.ap-south-1.rds.amazonaws.com",
+//   password: "87654321",
+//   database: "Nikola",
+// });
